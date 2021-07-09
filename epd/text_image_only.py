7f6fbc5ca5780f849c02701649c2b5a400a023b2
Jake Crystal <461621+AwesomeTac0@users.noreply.github.com>
2021-07-09 17:20:57 -0400

    Quick fix.

--- epd/text_image_only.py
+++ epd/text_image_only.py
@@ -15,66 +15,70 @@ class Display:
 
     def update_display(self, local_weather):
 
-        d = display_io.time_return(True)
+        if local_weather.full_update_needed:
 
-        display_io.clear()
-        print(f'Show Alarm: {local_weather.alerts}')
-        print('-'*20) # Makes 20 dashes, fancy!
-        print(d)
-        print(f'Temp: {str(round(local_weather.tempeture))}°F')
-        print(f'Wind Speed: {local_weather.wind_speed} Wind Direction: {display_io.return_wind_direction(local_weather.wind_direction)}')
-        print(f'Precipitation Probability: {local_weather.precipitation_probability}')
-        print(f'ico{display_io.slash}{display_io.code_to_weather(local_weather.weather_code)}.png')
-        print(f'Current conditions: {display_io.code_to_weather(local_weather.weather_code).replace("_"," ")}')
+            d = display_io.time_return(False)
 
-        try:
-            
-            if local_weather.save_image == True:
+            display_io.clear()
+            print(f'Show Alarm: {local_weather.alerts}')
+            print('-'*20) # Makes 20 dashes, fancy!
+            print(d)
+            print(f'Temp: {str(round(local_weather.tempeture))}°F')
+            print(f'Wind Speed: {local_weather.wind_speed} Wind Direction: {display_io.return_wind_direction(local_weather.wind_direction)}')
+            print(f'Precipitation Probability: {local_weather.precipitation_probability}')
+            print(f'ico{display_io.slash}{display_io.code_to_weather(local_weather.weather_code)}.png')
+            print(f'Current conditions: {display_io.code_to_weather(local_weather.weather_code).replace("_"," ")}')
 
-                # Create blank image
-                image = self.blank_image.copy()
-                draw = ImageDraw.Draw(image)
+            try:
+                
+                if local_weather.save_image == True:
 
-                # Top left - Date and Time
-                draw.text((2, 0), d, font = self.font_16, fill = 0)
+                    # Create blank image
+                    image = self.blank_image.copy()
+                    draw = ImageDraw.Draw(image)
 
-                # Left Mid - Alarm
-                if local_weather.alerts:
-                    draw.rectangle([(1,42),(75,66)],fill = 0)
-                    draw.text((2, 45), u'ALERT!', font = self.font_20, fill = 1)
+                    # Top left - Date and Time
+                    draw.text((2, 0), d, font = self.font_16, fill = 0)
 
-                # Left Mid - Wind and Direction
-                draw.text((2, 70), f'W: {round(local_weather.wind_speed)} MPH | D: {display_io.return_wind_direction(local_weather.wind_direction)}', font = self.font_16, fill = 0)
+                    # Left Mid - Alarm
+                    if local_weather.alerts:
+                        draw.rectangle([(1,42),(75,66)],fill = 0)
+                        draw.text((2, 45), u'ALERT!', font = self.font_20, fill = 1)
 
-                # Bottom Left - Current weather condition
-                draw.text((2, 100), display_io.code_to_weather(local_weather.weather_code).replace("_"," "), font = self.font_20, fill = 0)
+                    # Left Mid - Wind and Direction
+                    draw.text((2, 70), f'W: {round(local_weather.wind_speed)} MPH | D: {display_io.return_wind_direction(local_weather.wind_direction)}', font = self.font_16, fill = 0)
 
-                # Load weather icon
-                imgIco = display_io.dict_of_images[f'ico{display_io.slash}{display_io.code_to_weather(local_weather.weather_code)}.png']
+                    # Bottom Left - Current weather condition
+                    draw.text((2, 100), display_io.code_to_weather(local_weather.weather_code).replace("_"," "), font = self.font_20, fill = 0)
 
-                # Top Right - Insert weather icon
-                image.paste(imgIco, (185,0), imgIco)
+                    # Load weather icon
+                    imgIco = display_io.dict_of_images[f'ico{display_io.slash}{display_io.code_to_weather(local_weather.weather_code)}.png']
 
-                # Precipitation allignment correction
-                space = ''
-                if len(str(round(local_weather.precipitation_probability))) == 1:
-                    space = '   '
-                elif len(str(round(local_weather.precipitation_probability))) == 2:
-                    space = ' '
-                else:
+                    # Top Right - Insert weather icon
+                    image.paste(imgIco, (185,0), imgIco)
+
+                    # Precipitation allignment correction
                     space = ''
+                    if len(str(round(local_weather.precipitation_probability))) == 1:
+                        space = '   '
+                    elif len(str(round(local_weather.precipitation_probability))) == 2:
+                        space = ' '
+                    else:
+                        space = ''
 
-                # Right Mid - Precipitation
-                draw.text((188, 70), f'P: {space}{str(round(local_weather.precipitation_probability))} %', font = self.font_16, fill = 0)
+                    # Right Mid - Precipitation
+                    draw.text((188, 70), f'P: {space}{str(round(local_weather.precipitation_probability))} %', font = self.font_16, fill = 0)
 
-                # Bottom Right - Large Temp
-                draw.text((180, 95), ' ' + str(round(local_weather.tempeture)) + '°F', font = self.font_24, fill = 0)
+                    # Bottom Right - Large Temp
+                    draw.text((180, 95), ' ' + str(round(local_weather.tempeture)) + '°F', font = self.font_24, fill = 0)
 
-                # Correct oreintation
-                image = image.rotate(180)
+                    # Correct oreintation
+                    image = image.rotate(180)
 
-                image.save('Weather.jpg')
+                    image.save('Weather.jpg')
 
-        except Exception as e:
-            print('There was an error updating the screen.')
-            input(e)
+            except Exception as e:
+                print('There was an error updating the screen.')
+                input(e)
+
+            local_weather.full_update_needed = False
